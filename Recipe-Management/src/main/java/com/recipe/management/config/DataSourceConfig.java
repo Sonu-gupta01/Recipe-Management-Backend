87d1280e08d9be5455d20f33f85cf3af49bf891d
Sonugupta010 <sonurg010@gmail.com>
2024-12-18 11:12:13 +0530

    credential issue fixed

--- Recipe-Management/src/main/java/com/recipe/management/config/DataSourceConfig.java
+++ Recipe-Management/src/main/java/com/recipe/management/config/DataSourceConfig.java
@@ -1,34 +1,3 @@
-//package com.recipe.management.config;
-//
-//import com.zaxxer.hikari.HikariConfig;
-//import com.zaxxer.hikari.HikariDataSource;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import javax.sql.DataSource;
-//
-//@Configuration
-//public class DataSourceConfig {
-//
-//    @Bean
-//    public DataSource dataSource() {
-//        HikariConfig hikariConfig = new HikariConfig();
-//        hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/recipe_management");
-//        hikariConfig.setUsername("root");
-//        hikariConfig.setPassword("root");
-//        hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
-//
-//        // Additional HikariCP properties can be set here if needed
-//        hikariConfig.addDataSourceProperty("cachePrepStmts", "true");
-//        hikariConfig.addDataSourceProperty("prepStmtCacheSize", "250");
-//        hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-//        hikariConfig.addDataSourceProperty("useServerPrepStmts", "true");
-//
-//        return new HikariDataSource(hikariConfig);
-//    }
-//}
-//
-
 package com.recipe.management.config;
 
 import com.zaxxer.hikari.HikariConfig;
--- Recipe-Management/src/main/java/com/recipe/management/config/MailConfig.java
+++ Recipe-Management/src/main/java/com/recipe/management/config/MailConfig.java
@@ -1,32 +1,5 @@
 package com.recipe.management.config;
 
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.mail.javamail.JavaMailSenderImpl;
-//
-//import java.util.Properties;
-//
-//@Configuration
-//public class MailConfig {
-//
-//    @Bean
-//    public JavaMailSender javaMailSender() {
-//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-//        mailSender.setHost("smtp.gmail.com");
-//        mailSender.setPort(587);
-//        mailSender.setUsername("sonugupta.klal@gmail.com");
-//        mailSender.setPassword("tryoeznsbasalgjo");
-//
-//        Properties properties = mailSender.getJavaMailProperties();
-//        properties.put("mail.smtp.auth", "true");
-//        properties.put("mail.smtp.starttls.enable", "true");
-//        properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-//
-//        return mailSender;
-//    }
-//}
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
